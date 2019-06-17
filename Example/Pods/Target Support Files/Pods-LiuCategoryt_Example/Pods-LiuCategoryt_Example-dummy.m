2ff9e2e03dfd0c7eda5643e8da7963825c6b58e3
liukang <liuk@izkml.com>
2019-06-17 09:24:28 +0800

    编辑spec文件

---
+++ Example/Pods/Target Support Files/Pods-LiuCategoryt_Example/Pods-LiuCategoryt_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiuCategoryt_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LiuCategoryt_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-LiuCategoryt_Tests/Pods-LiuCategoryt_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiuCategoryt_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LiuCategoryt_Tests
+@end
